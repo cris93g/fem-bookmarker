cd5700ce8fb0e2f0c5cfb12fc97e37995d1a28e7
cris93g <37388115+cris93g@users.noreply.github.com>
2019-01-25 19:48:11 -0600

    adds bookmarks to page for any url you put

--- app/main.js
+++ app/main.js
@@ -1,9 +1,20 @@
 const { app, BrowserWindow } = require("electron");
+let mainWindow = null;
 
 app.on("ready", () => {
-	const mainWindow = new BrowserWindow({
+	mainWindow = new BrowserWindow({
 		width: 300,
-		height: 600
+		height: 600,
+		show: false
 	});
+
+	mainWindow.once("ready-to-show", () => {
+		mainWindow.show();
+	});
+
+	mainWindow.on("closed", () => {
+		mainWindow = null;
+	});
+
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
